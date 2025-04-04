5af3dc8e3bfd0af7623469819dedfc33697fd96f
Bilal Ansari <m.bilal.ansari.qadri.786@gmail.com>
2025-04-04 19:20:31 +0500

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Respond from "./Respond.jsx";
 import Home from "./Home.jsx";
 
